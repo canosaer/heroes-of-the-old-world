cc3d029a1af1ff4ca0e156bc263563d1123a1188
PaignMgmT <133676380+PaignMgmT@users.noreply.github.com>
2023-12-01 11:59:00 -0500

    attribute ratings

--- components/create/Traits.tsx
+++ components/create/Traits.tsx
@@ -1,15 +1,47 @@
 import { useState, useEffect, useContext } from 'react';
 import { Context } from '../../context/store';
 import { Box } from '@mui/system';
-import { Typography, Stack, TextField, Card, CardContent, CardMedia, IconButton } from '@mui/material';
+import { Typography, Stack, TextField, Card, CardContent, CardMedia, IconButton, Rating } from '@mui/material';
+import StarIcon from '@mui/icons-material/Star';
 
 export default function Traits() {
     const [ store, dispatch ] = useContext(Context);
+    const [value, setValue] = useState<number | null>(1);
+    const [hover, setHover] = useState(-1);
+  
+
+    const labels: { [index: string]: string } = {
+        1: 'Poor',
+        2: 'Average',
+        3: 'Good',
+        4: 'Excellent',
+        5: 'Legendary',
+    };
+
+    const getLabelText = (value: number) => {
+        return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+    }
 
     return (
         <Stack className="traits" component="form" noValidate autoComplete="off">
-            <Card className="agility">
-                <Typography>BLAH!</Typography>
+            <Card className="attribute">
+                <Typography className="attribute__heading">Agility</Typography>
+                <Rating
+                    name="hover-feedback"
+                    value={value}
+                    precision={1}
+                    getLabelText={getLabelText}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                    onChangeActive={(event, newHover) => {
+                        setHover(newHover);
+                    }}
+                    emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+                />
+                {value !== null && (
+                    <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+                )}
             </Card>
         </Stack>
     )
