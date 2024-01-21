5223b1b836b6233e7681885205eb82fa727027b2
canosaer <canosaer@gmail.com>
2024-01-21 16:15:49 -0500

    added spell mods

---
+++ data/characters/spellAttackMods.ts
@@ -0,0 +1,17 @@
+export const spellAttackMods = [
+  {
+    name: "Armor Piercing",
+    difficulty: "1-3",
+    summary: "The attack is focused to defeat armor or seeks out a foe's exposed areas. Each additional difficulty point grants the power AP 2, to a maximum of AP 6.",
+  },
+  {
+    name: "Fatigue",
+    difficulty: "2",
+    summary: "If hit, target also suffers Fatigue. This cannot cause Incapacitation.",
+  },
+  {
+    name: "Selective",
+    difficulty: "1",
+    summary: "With intense focus, the caster can choose not to affect any or all individual targets within a spell's area of effect.",
+  },
+]
